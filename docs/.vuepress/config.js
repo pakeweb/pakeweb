423676f46033e511b7c1861d745680e1373cac11
Eko Eryanto <ekoeryanto@gmail.com>
2018-09-05 10:38:42 +0700

    chore: initialize with simple usage

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+// use config.yml for simple configuration
+module.exports = {
+  dest: 'dist',
+  title: 'Vuepress',
+  description: 'Vue-powered Static Site Generator',
+  themeConfig: {
+    logo: 'https://vuepress.vuejs.org/hero.png',
+  },
+}

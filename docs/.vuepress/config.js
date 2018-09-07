bb65bb71375b99a8ccc8614b916de3196d21807b
Eko Eryanto <ekoeryanto@gmail.com>
2018-09-07 09:34:37 +0700

    feat: enable service worker

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,49 @@ module.exports = {
   dest: 'dist',
   title: 'PakeWeb',
   description: 'Website gratis mutakhir, cepat dan profesional.',
+  head: [
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+    ],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    [
+      'link',
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+    ],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: '#008080' },
+    ],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'PakeWeb' }],
+    ['meta', { name: 'application-name', content: 'PakeWeb' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#00aba9' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+  ],
+  serviceWorker: true,
   themeConfig: {
     search: false,
     repo: 'pakeweb/pakeweb',
@@ -16,5 +59,11 @@ module.exports = {
       // { text: 'hubungi', link: '/hubungi/' },
     ],
     activeHeaderLinks: false,
+    serviceWorker: {
+      updatePopup: {
+        message: 'Ditemukan pembaruan pada website.',
+        buttonText: 'Perbarui',
+      },
+    },
   },
 }

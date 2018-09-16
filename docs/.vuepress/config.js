4f8e0a2b3faa33f08dbe18e634e283c817b3a036
Eko Eryanto <ekoeryanto@gmail.com>
2018-09-17 01:48:29 +0700

    chore: move link after meta

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,15 @@ const path = require('path')
 module.exports = {
   dest: 'dist',
   head: [
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: '#008080' },
+    ],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'Pake Web' }],
+    ['meta', { name: 'application-name', content: 'Pake Web' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#00aba9' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
     [
       'link',
       {
@@ -35,15 +44,6 @@ module.exports = {
       'link',
       { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
     ],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    [
-      'meta',
-      { name: 'apple-mobile-web-app-status-bar-style', content: '#008080' },
-    ],
-    ['meta', { name: 'apple-mobile-web-app-title', content: 'Pake Web' }],
-    ['meta', { name: 'application-name', content: 'Pake Web' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#00aba9' }],
-    ['meta', { name: 'theme-color', content: '#ffffff' }],
     [
       'link',
       {
